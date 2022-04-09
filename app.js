1a5f2b2a7eebb91a34c0d16fcc51a3d3183f2bae
Astrit Spancs <astritspancs@Astrits-MacBook-Pro.local>
2022-04-09 18:54:26 +0200

    Prettier config

--- app.js
+++ app.js
@@ -1,28 +1,28 @@
-const DARK = "#000";
-const WHITE = "#fff";
+const DARK = '#000';
+const WHITE = '#fff';
 
 let defaultTheme = WHITE;
 
-document.getElementById("btn-theme-change").addEventListener("click", () => {
-  document.getElementById("container").style.backgroundColor =
+document.getElementById('btn-theme-change').addEventListener('click', () => {
+  document.getElementById('container').style.backgroundColor =
     defaultTheme === DARK ? WHITE : DARK;
-  document.getElementById("container").style.color =
+  document.getElementById('container').style.color =
     defaultTheme === DARK ? WHITE : DARK;
   defaultTheme = defaultTheme === DARK ? WHITE : DARK;
 });
 
 if (window.Worker) {
-  const worker = new Worker("./worker.js");
+  const worker = new Worker('./worker.js');
 
   document
-    .getElementById("btn-start-calculate")
-    .addEventListener("click", () => {
-      worker.postMessage("Start Calculation");
+    .getElementById('btn-start-calculate')
+    .addEventListener('click', () => {
+      worker.postMessage('Start Calculation');
     });
 
   worker.onmessage = (event) => {
     console.log(event.data);
   };
 } else {
-  console.log("Your browser doesn't support web workers");
+  console.log('Your browser doesn\'t support web workers');
 }
